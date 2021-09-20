8fdca20dc24f5e1f2aeec277bf5358dfc5c21184
Maxim Zorin <maxim.zorin@zodiacsystems.com>
2021-09-20 20:13:27 +0300

    fix: javascriptify

--- ru/js/index.js
+++ ru/js/index.js
@@ -1,3 +1,21 @@
+const app = {
+  title: 'CV Maxim Zorin -- Lead JavaScript Developer',
+  content: [
+    this.header = `<header></header>`,
+    this.cv = `<article id="cv"></article>`,
+    this.footer = `<footer>
+      <img src="assets/rs_school_js.svg" width="100px">
+      <div>
+        <a href="https://github.com/mxn42/rsschool-cv">mxn42/rsschool-cv</a> |
+        2021 |
+        <a href="https://rs.school/js/">JS Course</a>
+      </div>
+      </a>
+    </footer>`
+  ]
+}
+document.title = app.title;
+app.content.forEach(x => document.body.insertAdjacentHTML('beforeend', x));
 const md = markdownit({
   html: true,
   linkify: true,
