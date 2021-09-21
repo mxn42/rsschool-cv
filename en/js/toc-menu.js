e1687f0f011b954c01f83ace6025e6f2496549db
Maxim Zorin <maxim.zorin@zodiacsystems.com>
2021-09-21 05:01:35 +0300

    fix: simplify links

--- en/js/toc-menu.js
+++ en/js/toc-menu.js
@@ -2,11 +2,10 @@ class TocMenu {
   constructor() {
     const elements = [...document.querySelectorAll('[data-toc-label]')];
     const ul = this.ul = document.createElement('ul');
-    let i = 0;
     for (let el of elements) {
-      const tocId = `toc${++i}`;
-      el.setAttribute('id', tocId);
-      ul.insertAdjacentHTML('beforeend', `<li><a href="#${tocId}">${el.getAttribute('data-toc-label')}</a></li>`);
+      const label = el.getAttribute('data-toc-label');
+      el.setAttribute('id', label);
+      ul.insertAdjacentHTML('beforeend', `<li><a href="#${label}">${label}</a></li>`);
     }
   }
 
