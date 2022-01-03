68623cc9934bfd09eb5dac06678c48008a65a04a
Maxim Zorin <maxim.zorin@dev.zodiac.tv>
2022-01-03 03:57:47 +0300

    feat: add nav menu

---
+++ lite/en/js/toc-menu.js
@@ -0,0 +1,22 @@
+function tocMenu() {
+  const elements = [...document.querySelectorAll('[data-toc-label]')];
+  const ul = document.createElement('ul');
+  for (let el of elements) {
+    const label = el.dataset.tocLabel;
+    const icon = el.dataset.tocIcon;
+    const id = label.replace(' ', '');
+    el.setAttribute('id', id);
+    const li = ul.appendChild(document.createElement('li'));
+    const a = li.appendChild(document.createElement('a'));
+    a.insertAdjacentHTML('afterbegin', `<i class="bx ${icon}"></i> <span class="name">${label}</span>`);
+    a.setAttribute('title', label);
+    a.setAttribute('href', `#${id}`);
+    a.addEventListener('click', e => {
+      e.preventDefault();
+      document.getElementById(id).scrollIntoView({behavior: 'smooth', block: 'start'});
+    });
+  }
+  return ul;
+};
+
+document.querySelector('.body-header nav').appendChild(tocMenu());
